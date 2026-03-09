f8e99984c224fd251986dc2da9ad6fa470f29857
Vahin M <vahin@vahin.dev>
2026-03-09 15:28:48 +0000

    i really hate react never wanna touch this ever again

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,50 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  NavLink
+} from "react-router-dom";
+
+const Other = () => (
+  <Layout>
+    <NavLink to="/">Home</NavLink>
+    <h1>AnotherPage!</h1>
+  </Layout>
+);
+
+const Layout = ({ children }) => {
+  const { pathname } = useLocation();
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div>
+      <div>{children}</div>
+    </div>
+  );
+};
+
+const Home = () => (
+  <Layout>
+    <ul>
+      <li><NavLink to = "/other" className={"header-link"}>Other</NavLink></li>
+    </ul>
+    <main>
+      <h1>Vahin's Website</h1>
+      <p>Hi, I'm Vahin, a 17 year old software developer with delusions of grandeur</p>
+    </main>
+  </Layout>
+);
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/other" element={<Other />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
