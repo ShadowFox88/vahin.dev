edf781ae5f5642560ea9d79aa3ccdede19e4c3f8
Vahin M <vahin@vahin.dev>
2026-05-06 18:49:00 +0000

    refactor: added text to canvas rather than render seperately

--- components/common/Background.tsx
+++ components/common/Background.tsx
@@ -1,22 +1,16 @@
 "use client"
 
 import { useEffect, useRef, useState } from "react";
-// hi there's a chat at the bottom
-// please click
-// if it says blocked pls copy link from devtools network and open it
-// then try again
-// nithi say hi if you see this
-export default function Background() {
-    let bg_list = [];
-    const canvasRef = useRef(null);
-    const [isClient, setIsClient] = useState(false)
 
-    const svg = <svg width="64" height="64" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" shapeRendering="crispEdges">
-        <line x1="0" x2="100" y1="50" y2="50" stroke="white" strokeWidth="1" opacity="0.4" />
-        <line x1="50" x2="50" y1="0" y2="100" stroke="white" strokeWidth="1" opacity="0.4" />
-        <line x1="42.5" x2="57.5" y1="50" y2="50" stroke="white" strokeWidth="3" opacity="0.4" />
-        <line x1="50" x2="50" y1="42.5" y2="57.5" stroke="white" strokeWidth="3" opacity="0.4" />
-    </svg>
+export default function Background() {
+    const canvasRef = useRef<HTMLCanvasElement | null>(null);
+
+    // const svg = <svg width="64" height="64" viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg" shapeRendering="crispEdges">
+    //     <line x1="0" x2="100" y1="50" y2="50" stroke="white" strokeWidth="1" opacity="0.4" />
+    //     <line x1="50" x2="50" y1="0" y2="100" stroke="white" strokeWidth="1" opacity="0.4" />
+    //     <line x1="42.5" x2="57.5" y1="50" y2="50" stroke="white" strokeWidth="3" opacity="0.4" />
+    //     <line x1="50" x2="50" y1="42.5" y2="57.5" stroke="white" strokeWidth="3" opacity="0.4" />
+    // </svg>
 
     const drawCell = (ctx: any, x: number, y: number, size: number) => {
         const ratio = window.innerWidth/2560
@@ -47,10 +41,10 @@ export default function Background() {
     };
 
     useEffect(() => {
-        setIsClient(true)
-
         const canvas = canvasRef.current;
+        if (canvas == null) return;
         const ctx = canvas.getContext("2d");
+        if (ctx == null) return;
 
         ctx.imageSmoothingEnabled= false
 
@@ -64,6 +58,19 @@ export default function Background() {
             for (let x = 0; x < w; x += size) {
                 for (let y = 0; y < h; y += size) {
                     drawCell(ctx, x, y, size);
+                }
+            }
+
+            ctx.globalAlpha = 0.2;
+            ctx.fillStyle = "white";
+
+            // background letters
+            let char_size = 25000 / (window.innerWidth)
+            for (let x = 1; x < w; x = x + char_size) {
+                for (let y = 0; y < h; y += char_size + 4) {
+                    let char = Math.random().toString(36).toUpperCase().substring(2, 3);
+                    ctx.font = `${char_size}px monospace`;
+                    ctx.fillText(char, x, y);
                 }
             }
         };
@@ -96,31 +103,10 @@ export default function Background() {
         return () => window.removeEventListener("resize", resize);
     }, []);
 
-    for (let i = 0; i < 5000; i++) {
-        bg_list.push(Math.random().toString(36).toUpperCase().substring(2, 3));
-    }
-
-    return <div className="max-h-screen overflow-hidden fixed inset-0 -z-10">
-        <div className="flex flex-wrap text-[4px] text-white opacity-40">
-            {bg_list.map((char, index) => <div key={char + index.toString() + "_bg"} className="p-2">{isClient ? char : null}</div>)}
+    return <div className="max-h-screen overflow-hidden fixed inset-0 -z-10 blur-[2px]">
+        <div className="flex flex-wrap text-[4px] text-white opacity-40" id="text-bg">
+            
         </div>
-        <canvas className="fixed inset-0 -z-10 pointer-events-none" ref={canvasRef} style={{
-            image-rendering: "pixelated";
-            image-rendering: "crisp-edges";
-
-        }} />
+        <canvas className="fixed inset-0 -z-10 pointer-events-none" ref={canvasRef} />
     </div>
 }
-// "use client"
-
-// 
-
-// export default function Background() {
-
-//   return (
-//     <canvas
-//       ref={canvasRef}
-//       className=""
-//     />
-//   );
-// }
